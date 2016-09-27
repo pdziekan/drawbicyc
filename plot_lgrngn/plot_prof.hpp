1e9539bbe9656950291c12481be98be3ad06eff1
pdziekan <pdziekanp@gmail.com>
2016-09-27 15:23:29 +0200

    one program for plotting

--- plot_lgrngn/plot_prof.hpp
+++ plot_lgrngn/plot_prof.hpp
@@ -536,27 +536,3 @@ void plot_profiles(Plotter_t plotter)
   oprof_file << z_i << std::endl;
 }
 
-int main(int ac, char** av)
-{
-  if (ac != 2) error_macro("expecting 1 argument: dir containing out_lgrngn")
-
-  std::string
-    dir = string(av[1]),
-    h5  = dir + "out_lgrngn";
-
-  // detecting input data dimensionality
-  H5::H5File h5f(h5 + "/const.h5", H5F_ACC_RDONLY);
-  H5::DataSet h5d = h5f.openDataSet("G");
-  H5::DataSpace h5s = h5d.getSpace();
-  int NDims = h5s.getSimpleExtentNdims();
-
-  if(NDims == 2)
-    plot_profiles(Plotter_t<2>(h5));
-  else if(NDims == 3)
-    plot_profiles(Plotter_t<3>(h5));
-  else
-    assert(false && "need 2d or 3d input data");
-
-return 0;
-} // main
-
