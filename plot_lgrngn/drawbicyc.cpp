a642272fd3a925c25ae6eb79ce4701a9db3ef1d9
pdziekan <pdziekanp@gmail.com>
2016-09-27 15:56:52 +0200

    program opts

--- plot_lgrngn/drawbicyc.cpp
+++ plot_lgrngn/drawbicyc.cpp
@@ -1,13 +1,44 @@
 #include"plot_series.hpp"
 #include"plot_prof.hpp"
+// command-line option handling
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+namespace po = boost::program_options;
 
-int main(int ac, char** av)
+// some globals for option handling
+int ac;
+char** av; 
+po::options_description opts_main("General options");
+
+
+int main(int argc, char** argv)
 {
-  if (ac != 2) error_macro("expecting 1 argument: dir containing out_lgrngn")
+  // make args global
+  ac=argc;
+  av=argv;
 
+  // general opts
+  opts_main.add_options()
+    ("prof", po::value<bool>()->default_value(true), "plot profiles?")
+    ("series", po::value<bool>()->default_value(true) , "plot series?")
+    ("dir", po::value<std::string>()->required() , "directory containing out_lgrngn")
+  ;
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(ac, av).options(opts_main).allow_unregistered().run(), vm); //     ignores unknown
+
+  // checking if all required options present
+  po::notify(vm);
+
+  // parse dir name
   std::string
-    dir = string(av[1]),
+    dir = vm["dir"].as<std::string>(),
     h5  = dir + "out_lgrngn";
+
+  // reading required plot types
+  bool flag_series = vm["series"].as<bool>(),
+       flag_profiles = vm["prof"].as<bool>();
 
   // detecting input data dimensionality
   H5::H5File h5f(h5 + "/const.h5", H5F_ACC_RDONLY);
@@ -17,13 +48,13 @@ int main(int ac, char** av)
 
   if(NDims == 2)
   {
-    plot_series(Plotter_t<2>(h5));
-    plot_profiles(Plotter_t<2>(h5));
+    if(flag_series)   plot_series(Plotter_t<2>(h5));
+    if(flag_profiles) plot_profiles(Plotter_t<2>(h5));
   }
   else if(NDims == 3)
   {
-    plot_series(Plotter_t<3>(h5));
-    plot_profiles(Plotter_t<3>(h5));
+    if(flag_series)   plot_series(Plotter_t<3>(h5));
+    if(flag_profiles) plot_profiles(Plotter_t<3>(h5));
   }
   else
     assert(false && "need 2d or 3d input data");
